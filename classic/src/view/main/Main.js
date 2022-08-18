710563cbf441ff4a4739fa6bc11fe8932e64afe8
Elvis Taco <etaco@gruporoi.pe>
2022-08-18 13:54:03 -0500

    titulos cambiados

--- classic/src/view/main/Main.js
+++ classic/src/view/main/Main.js
@@ -5,117 +5,113 @@
  *
  * TODO - Replace this content of this view to suite the needs of your application.
  */
-Ext.define('MyApp.view.main.Main', {
-    extend: 'Ext.tab.Panel',
-    xtype: 'app-main',
+Ext.define("MyApp.view.main.Main", {
+  extend: "Ext.tab.Panel",
+  xtype: "app-main",
 
-    requires: [
-        'Ext.plugin.Viewport',
-        'Ext.window.MessageBox',
+  requires: [
+    "Ext.plugin.Viewport",
+    "Ext.window.MessageBox",
 
-        'MyApp.view.main.MainController',
-        'MyApp.view.main.MainModel',
-        'MyApp.view.main.List',
-        'MyApp.view.main.Ventas'
-    ],
+    "MyApp.view.main.MainController",
+    "MyApp.view.main.MainModel",
+    "MyApp.view.main.List",
+    "MyApp.view.main.Ventas",
+  ],
 
-    controller: 'main',
-    viewModel: 'main',
+  controller: "main",
+  viewModel: "main",
 
-    ui: 'navigation',
+  ui: "navigation",
 
-    tabBarHeaderPosition: 1,
-    titleRotation: 0,
-    tabRotation: 0,
+  tabBarHeaderPosition: 1,
+  titleRotation: 0,
+  tabRotation: 0,
 
-    header: {
-        layout: {
-            align: 'stretchmax'
-        },
-        title: {
-            bind: {
-                text: '{name}'
-            },
-            flex: 0
-        },
-        iconCls: 'fa-th-list'
+  header: {
+    layout: {
+      align: "stretchmax",
     },
-
-    tabBar: {
-        flex: 1,
-        layout: {
-            align: 'stretch',
-            overflowHandler: 'none'
-        }
+    title: {
+      bind: {
+        text: "{name}",
+      },
+      flex: 0,
     },
+    iconCls: "fa-th-list",
+  },
 
-    responsiveConfig: {
-        tall: {
-            headerPosition: 'top'
-        },
+  tabBar: {
+    flex: 1,
+    layout: {
+      align: "stretch",
+      overflowHandler: "none",
+    },
+  },
+
+  responsiveConfig: {
+    tall: {
+      headerPosition: "top",
+    },
+    wide: {
+      headerPosition: "left",
+    },
+  },
+
+  defaults: {
+    bodyPadding: 0,
+    tabConfig: {
+      responsiveConfig: {
         wide: {
-            headerPosition: 'left'
-        }
-    },
-
-    defaults: {
-        bodyPadding: 0,
-        tabConfig: {
-            responsiveConfig: {
-                wide: {
-                    iconAlign: 'left',
-                    textAlign: 'left'
-                },
-                tall: {
-                    iconAlign: 'top',
-                    textAlign: 'center',
-                    width: 120
-                }
-            }
-        }
-    },
-  
-            fullscreen: true,
-    items: [{
-        title: 'Home',
-        iconCls: 'fa-home',
-        // The following grid shares a store with the classic version's grid as well!
-        scrollable : {
-            direction     : 'vertical',
-            directionLock : true
-      },
-        items: [{
-            xtype: 'mainlist'
-        }]
-    }, {
-        scrollable : {
-            direction     : 'vertical',
-            directionLock : true
-      },
-        title: ' Ventas',
-        iconCls: 'fa-users',
-        // The following grid shares a store with the classic version's grid as well!
-        layout: {
-            type: 'hbox',
-            pack: 'start',
-            align: 'start'
+          iconAlign: "left",
+          textAlign: "left",
         },
-        items: [{
-           
-            xtype: 'ventas',
-flex:1          
-        }]
-    }, {
-        title: 'Groups',
-        iconCls: 'fa-users',
-        bind: {
-            html: '{loremIpsum}'
-        }
-    }, {
-        title: 'Settings',
-        iconCls: 'fa-cog',
-        bind: {
-            html: '{loremIpsum}'
-        }
-    }]
+        tall: {
+          iconAlign: "top",
+          textAlign: "center",
+          width: 120,
+        },
+      },
+    },
+  },
+
+  fullscreen: true,
+  items: [
+    {
+      title: "trabajadores",
+      iconCls: "fa-home",
+      // The following grid shares a store with the classic version's grid as well!
+      scrollable: {
+        direction: "vertical",
+        directionLock: true,
+      },
+      items: [
+        {
+          xtype: "mainlist",
+        },
+      ],
+    },
+    {
+      scrollable: {
+        direction: "vertical",
+        directionLock: true,
+      },
+      title: " Ventas",
+      iconCls: "fa-users",
+      // The following grid shares a store with the classic version's grid as well!
+      layout: {
+        type: "hbox",
+        pack: "start",
+        align: "start",
+      },
+      items: [
+        {
+          xtype: "ventas",
+          flex: 1,
+        },
+      ],
+    },
+    
+   
+  ],
 });
